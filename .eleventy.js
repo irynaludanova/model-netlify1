d35d38c94eccaa8b3824004efd8e762379a3a7a4
Ludanova <irynaludanova@gmail.com>
2025-09-05 11:57:06 +0300

    Fixed url path

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,23 @@
+const fs = require("fs")
+const path = require("path")
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addCollection("profiles", function () {
+    const profilesDir = "./_data/static-profiles"
+    const files = fs.readdirSync(profilesDir).filter((f) => f.endsWith(".json"))
+
+    return files.map((file) => {
+      const profile = JSON.parse(fs.readFileSync(path.join(profilesDir, file)))
+      return {
+        data: { profile },
+        url: `/profiles/${profile.id}/`,
+      }
+    })
+  })
+
   eleventyConfig.addPassthroughCopy("img")
   eleventyConfig.addPassthroughCopy("css")
   eleventyConfig.addPassthroughCopy("favicon.ico")
-
-  eleventyConfig.addCollection("profiles", function () {
-    const profiles = require("./_data/static-profiles.js")()
-    return profiles.map((profile) => ({
-      data: { profile },
-      url: `/profiles/${profile.id}/`,
-    }))
-  })
 
   return {
     dir: {
