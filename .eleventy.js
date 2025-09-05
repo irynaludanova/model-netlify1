f824581d87a1d7b2293d942ce10c1cb205634fa2
Ludanova <irynaludanova@gmail.com>
2025-09-05 20:56:18 +0300

    added supabase config

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,10 @@ const fs = require("fs")
 const path = require("path")
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addCollection("profiles", async function (collectionApi) {
+  eleventyConfig.addCollection("profiles", async function () {
     const allProfiles = await require("./_data/all-profiles.js")()
-    return allProfiles.map((p) => ({
-      data: { profile: p },
-      url: p.url,
-    }))
+
+    return allProfiles.map((p) => ({ data: { profile: p }, url: p.url }))
   })
 
   eleventyConfig.addPassthroughCopy("img")
