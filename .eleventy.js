0f3cce2bae3a7f2018c61c50eb3ed404a1cc08e5
Ludanova <irynaludanova@gmail.com>
2025-09-07 15:31:43 +0300

    Fixed save data

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv"
 dotenv.config()
-import allProfiles from "./_data/all-profiles.js"
 
 export default function (eleventyConfig) {
   console.log(
@@ -9,17 +8,12 @@ export default function (eleventyConfig) {
   )
   console.log(
     "SUPABASE_SERVICE_ROLE_KEY:",
-    process.env.SUPABASE_SERVICE_ROLE_KEY ? "✅ задан" : "❌ пуст"
+    process.env.SUPABASE_SERVICE_ROLE_KEY ? "✅ задан" : "❌ пусто"
   )
 
   eleventyConfig.addGlobalData("env", {
     SUPABASE_URL: process.env.SUPABASE_URL || null,
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY || null,
-  })
-
-  eleventyConfig.addCollection("profiles", async function () {
-    const profiles = await allProfiles()
-    return profiles
   })
 
   eleventyConfig.addPassthroughCopy("img")
