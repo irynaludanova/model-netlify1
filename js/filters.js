732c7449016c7af33b0ebea78f4bea390b06dab2
Ludanova <irynaludanova@gmail.com>
2025-09-12 18:19:43 +0300

    fix slugify

--- js/filters.js
+++ js/filters.js
@@ -1,5 +1,4 @@
-// filters.js
-import slugify from "slugify"
+import slugify from "https://cdn.jsdelivr.net/npm/slugify@1.6.6/slugify.min.js"
 
 export function initFilters(profilesData) {
   const profileList = document.getElementById("profile-list")
